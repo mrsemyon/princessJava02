6567c05646b6f84d39cb213dc936738857040ba4
Morozov Semyon <sky_net@mail.ru>
2024-04-25 18:05:43 +0500

    added 2nd exercise solution

--- src/main/java/com/github/mrsemyon/Main.java
+++ src/main/java/com/github/mrsemyon/Main.java
@@ -1,7 +1,24 @@
 package com.github.mrsemyon;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(10, 10, 20, 30, 40, 50, 51));
+        System.out.println(numbers);
+        System.out.println("Is array contains duplicate number: " + isDuplicateNumber(numbers));
+    }
+    public static String isDuplicateNumber(ArrayList<Integer> numbers){
+        Set<Integer> newNumbers = new HashSet<Integer>();
+        for (int i = 0; i < numbers.size(); i++) {
+            if (!newNumbers.contains(numbers.get(i))){
+                newNumbers.add(numbers.get(i));
+            } else
+                return "true";
+        }
+        return "false";
     }
 }
